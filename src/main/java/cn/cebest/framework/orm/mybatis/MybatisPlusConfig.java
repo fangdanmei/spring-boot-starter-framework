d27ae2b36afa5a20001abb27df002f3bb868f5b4
maming <maming@300.cn>
2018-08-28 15:21:35 +0800

    添加mybatis-plus配置

---
+++ src/main/java/cn/cebest/framework/orm/mybatis/MybatisPlusConfig.java
@@ -0,0 +1,27 @@
+package cn.cebest.framework.orm.mybatis;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.baomidou.mybatisplus.plugins.PaginationInterceptor;
+
+
+/**
+ *  mybatis-plus配置
+  * @author maming  
+  * @date 2018年8月28日
+ */
+@Configuration
+@ConditionalOnProperty(prefix="gc.mybatisplus", value="enable", matchIfMissing = true)
+public class MybatisPlusConfig {
+
+	/**
+	 * 分页插件
+	 * @return
+	 */
+	@Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+
+}
