b0dd75b0b2c1be14e8ce13c6118de081256e26e6
maming <maming@300.cn>
2018-08-29 18:18:09 +0800

    添加参数验证组

---
+++ src/main/java/cn/cebest/framework/validator/group/CreateWish.java
@@ -0,0 +1,16 @@
+package cn.cebest.framework.validator.group;
+
+
+/**
+ *  参数校验组(创建时校验)
+  * @author maming  
+  * @date 2018年8月29日
+ */
+public interface CreateWish {
+
+	/**
+	 * 使用说明
+	 * 字段 : groups = {CreateWish.class}
+	 * 对象 : @Valid({CreateWish.class})
+	 */
+}
---
+++ src/main/java/cn/cebest/framework/validator/group/UpdateWish.java
@@ -0,0 +1,16 @@
+package cn.cebest.framework.validator.group;
+
+
+/**
+ *  参数校验组(更新时校验)
+  * @author maming  
+  * @date 2018年8月29日
+ */
+public interface UpdateWish {
+
+	/**
+	 * 使用说明
+	 * 字段 : groups = {UpdateWish.class}
+	 * 对象 : @Valid({UpdateWish.class})
+	 */
+}
