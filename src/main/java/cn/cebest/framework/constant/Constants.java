127781ba33d014458263cf517da127e374022805
maming <maming@300.cn>
2018-08-29 16:32:23 +0800

    丰富验证

--- src/main/java/cn/cebest/framework/constant/Constants.java
+++ src/main/java/cn/cebest/framework/constant/Constants.java
@@ -19,5 +19,25 @@ public class Constants {
 	/**
 	 * 手机号正则表达式
 	 */
-	public final static String PHONE_PATTERN = "^1[3456789]\\d{9}$";
+	public final static String REGEX_PHONE = "^1[3456789]\\d{9}$";
+	/**
+	* 正则表达式：验证邮箱
+	*/
+	public static final String REGEX_EMAIL = "^([a-z0-9A-Z]+[-|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$";
+	/**
+	* 正则表达式：验证汉字
+	*/
+	public static final String REGEX_CHINESE = "^[\u4e00-\u9fa5],{0,}$";
+	/**
+	* 正则表达式：验证身份证
+	*/
+	public static final String REGEX_ID_CARD = "(^\\d{18}$)|(^\\d{15}$)";
+	/**
+	* 正则表达式：验证URL
+	*/
+	public static final String REGEX_URL = "http(s)?://([\\w-]+\\.)+[\\w-]+(/[\\w- ./?%&=]*)?";
+	/**
+	* 正则表达式：验证IP地址
+	*/
+	public static final String REGEX_IP_ADDR = "(25[0-5]|2[0-4]\\d|[0-1]\\d{2}|[1-9]?\\d)";
 }
--- src/main/java/cn/cebest/framework/util/ValidatorUtil.java
+++ src/main/java/cn/cebest/framework/util/ValidatorUtil.java
@@ -1,6 +1,6 @@
 package cn.cebest.framework.util;
 
-import org.apache.commons.lang3.StringUtils;
+import java.util.regex.Pattern;
 import cn.cebest.framework.constant.Constants;
 
 /**
@@ -10,17 +10,65 @@ import cn.cebest.framework.constant.Constants;
  */
 public class ValidatorUtil {
 
+	/**
+	* 校验IP地址
+	* 
+	* @param ipAddr
+	* @return
+	*/
 	public static boolean isMobile(String mobile) {
-		
-		if (StringUtils.isEmpty(mobile)) {
-			return false;
-		}
+		return Pattern.matches(Constants.REGEX_PHONE, mobile);
+	}
+	
+	
+	/**
+	* 校验邮箱
+	* 
+	* @param email
+	* @return 校验通过返回true，否则返回false
+	*/
+	public static boolean isEmail(String email) {
+	return Pattern.matches(Constants.REGEX_EMAIL, email);
+	}
 
-		if (!mobile.matches(Constants.PHONE_PATTERN)) {
-			return false;
-		}
+	/**
+	* 校验汉字
+	* 
+	* @param chinese
+	* @return 校验通过返回true，否则返回false
+	*/
+	public static boolean isChinese(String chinese) {
+	return Pattern.matches(Constants.REGEX_CHINESE, chinese);
+	}
 
-		return true;
+	/**
+	* 校验身份证
+	* 
+	* @param idCard
+	* @return 校验通过返回true，否则返回false
+	*/
+	public static boolean isIDCard(String idCard) {
+	return Pattern.matches(Constants.REGEX_ID_CARD, idCard);
+	}
+
+	/**
+	* 校验URL
+	* 
+	* @param url
+	* @return 校验通过返回true，否则返回false
+	*/
+	public static boolean isUrl(String url) {
+	return Pattern.matches(Constants.REGEX_URL, url);
+	}
+
+	/**
+	* 校验IP地址
+	* 
+	* @param ipAddr
+	* @return
+	*/
+	public static boolean isIPAddr(String ipAddr) {
+	return Pattern.matches(Constants.REGEX_IP_ADDR, ipAddr);
 	}
 
 }
