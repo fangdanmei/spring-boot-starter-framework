441ede52bfc0486ab12bf32d8a7140470dd3d3c2
gaopeng <gaopeng@300.cn>
2018-08-29 13:28:18 +0800

    提供base支持。項目中使用的類需繼承此類

---
+++ src/main/java/cn/cebest/framework/support/BaseDao.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2010-2018 300.cn All Rights Reserved
+ *
+ * File:BaseDao.java Project: spring-boot-starter-framework
+ * 
+ * Creator: gaopeng@300.cn 
+ * Date:2018年8月29日 下午1:27:35
+ * 
+ */
+package cn.cebest.framework.support;
+
+/**
+ * @author gaopeng@300.cn
+ * @version 2018年8月29日 下午1:27:35
+ */
+public class BaseDao {
+
+}
---
+++ src/main/java/cn/cebest/framework/support/BaseService.java
@@ -0,0 +1,10 @@
+package cn.cebest.framework.support;
+
+/**
+ * 
+ * @author gaopeng@300.cn
+ * @version 2018年8月29日 下午1:26:19
+ */
+public class BaseService {
+
+}
